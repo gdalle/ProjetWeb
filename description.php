e6a355e40e1e6ebe172eded8ce1b1007bc78f5fc
Giom <guillaume.c.dalle@gmail.com>
2017-01-27 23:43:11 +0100

    Premier commit, pages statiques HTML-CSS

---
+++ description.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Guillaume Dalle"/>
+        <meta name="keywords" content="Mots clefs relatifs à cette page"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSS Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- CSS Perso -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <title>Cours de langues - Choix des cours</title>
+
+    </head>
+
+    <body>
+
+        <div class="container fluid">
+
+            <div class="row">
+                <div class="page header" id="up">
+                    <h1>Cours de langues
+                        <br>
+                        <small>Choix des cours</small>
+                    </h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <a href="index.php"><button type="button" class="btn btn-danger">Retour Assistant DE</button></a>
+            </div>
+
+            <br>
+
+            <div class="row">                    
+                <ul class="nav nav-pills">
+                    <li role="presentation"><a href="langues.php">Accueil</a></li>
+                    <li role="presentation" class="active"><a href="#">Choix des cours</a></li>
+                    <li role="presentation"><a href="affectation.php">Mon affectation</a></li>
+                </ul>
+            </div>
+
+            <br>
+
+            <div class="row" style="background-color: black; border-radius: 20pt">
+                <div class="col-md-2  col-sm-4 col-6">
+                    <br>
+                    <a href="#allemand"><img src="medias/all.png" alt="allemand" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2  col-sm-4 col-5">
+                    <br>
+                    <a href="#anglais"><img src="medias/ang.png" alt="anglais" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2  col-sm-4 col-6">
+                    <br>
+                    <a href="#arabe"><img src="medias/ara.png" alt="japonais" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2  col-sm-4 col-6">
+                    <br>
+                    <a href="#chinois"><img src="medias/chi.png" alt="chinois" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2  col-sm-4 col-6">
+                    <br>
+                    <a href="#espagnol"><img src="medias/esp.png" alt="espagnol" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <br>
+                    <a href="#francais"><img src="medias/fra.png" alt="japonais" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <br>
+                    <a href="#italien"><img src="medias/ita.png" alt="italien" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <br>
+                    <a href="#japonais"><img src="medias/jap.png" alt="japonais" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <br>
+                    <a href="#portugais"><img src="medias/por.png" alt="japonais" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+                <div class="col-md-2 col-sm-4 col-6">
+                    <br>
+                    <a href="#russe"><img src="medias/rus.png" alt="japonais" class="img-responsive img-rounded"></a>
+                    <br>
+                </div>
+
+            </div>
+
+            <br>
+
+<div class="row" id="allemand">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Allemand</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="row" id="anglais">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Anglais</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+                        <div id="ANG" class="panel-group">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"> 
+                                        <input type="checkbox" aria-label="...">
+                                        <a href="#ANG483" data-toggle="collapse" data-parent="#ANG"> ANG483 : Business Communication </a>
+                                    </h4>
+                                </div>
+                                <div id="ANG483" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Professeur : Kathryn English</li>
+                                            <li>Horaire : Mardi 18h-20h</li>
+                                            <li>Salle : 55</li>
+                                            <li>Contenu : 
+                                                <br>
+                                                This course involves exploring issues inherent to communication in the globalized business world. This means using English, not as a subject for study but as a tool for international and intercultural communication.  Students should take both modules three and four.  However it is possible to follow only one module.
+                                                We start with straightforward business problems/issues and present different perspectives. Students will be required to come up with and present creative solutions to hypothetical problems such as restructuring a failing airline or relocating a factory. Articles for reading will be set.
+                                                Students practice effective techniques for running or participating in meetings according to American procedures.  These will be presented and simulated.  Participants then present projects which are evaluated by hypothetical investors.
+                                                The different steps to negotiation (using Ury and Fischer’s work from the Harvard Negotiation Project) will be experienced via simulated projects with students working on separate teams. Teamwork is emphasized throughout the course. Strategies for effective brainstorming will also be presented.
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"> 
+                                        <input type="checkbox" aria-label="...">
+                                        <a href="#ANG464" data-toggle="collapse" data-parent="#ANG"> ANG464 : American Series </a>
+                                    </h4>
+                                </div>
+                                <div id="ANG464" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Professeur : Daivy Babel</li>
+                                            <li>Horaire : Lundi 18h-20h</li>
+                                            <li>Salle : 57</li>
+                                            <li>Contenu : 
+                                                <br>
+                                                What made you think that NBC’s The Blacklist was original when you watched the very first episode? What do you mean when you say AMC's The Walking Dead is a success? What are you comparing it with/ to? How does CBS’s Person of Interest really differ in tone and atmosphere from The Wire, 24 and Homeland? Can Modern Family be compared to any other show? Perhaps, in a way, to Diff'rent Strokes (1978- 1985/1986) or The Cosby Show (1984-1992)? Have you ever seen a pattern in Prison Break/ing Bad or in Dexter? Does the British period drama television series Downton Abbey display, from a historical perspective, much originality? More than the nth superhero depiction of The Flash? – how original can a series still be after the eponymous comic books, movies, comic strips and … the American superhero TV series from 1990? 
+                                                Repetition and variation are key to understanding serial TV shows; this means comparison is the first and foremost instrument to be used in analyzing them. Opposing pattern to originality will be the main focus of our studies. To gain better insights into the making of a series, but also into its cultural content as well as its impact - in particular its ideological impact - comparison can be of paramount importance. Comparing two or more cultural objects from various perspectives and on several levels can increase the number of angles from which those series can be read and understood, and can consequently lead to new and original perceptions of the series.  Beyond the comparisons per se, you will be encouraged to question the concept of comparison itself, that is to say to get perspective vis-à-vis your methodology and instruments of analysis. Can one draw conclusions when comparing two or more series from different decades? Is it useful / possible to compare two different genres at two different times? Can such an intellectual instrument of analysis and its scientific methodology help when it comes to series that you like / love / hate? 
+                                                ‘On the The Big Bang Theory, how can we characterize the evolution of Bernadette Maryann Rostenkowski between her introduction in The Creepy Candy Coating Corollary and the finale of season 5, when she becomes Mrs Rostenkowski-Wolowitz? Is it parallel to that of Amy Farrah Fowler? To what extent are spoilers a much more significant problem when it comes to Game of Thrones than when it comes to Friends? Does it make more sense to compare the latter to its spin-off Joey than to Courtney Cox’s Cougar Town or to another sitcom? Does collating The Big Bang Theory (2007-) and Friends (1994-2004) show endless repetition or an evolution in sitcoms over two decades?’ are examples of questions students can ask themselves and that will lead to oral presentations, debates & discussions in class where communications skills will be sharpened thanks to regular participation. 
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row" id="arabe">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Arabe</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="chinois">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Chinois</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="espagnol">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Espagnol</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="francais">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Français (langue étrangère)</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="italien">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Italien</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="japonais">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Japonais</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="portugais">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Portugais</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row" id="russe">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h2>Russe</h2> 
+                        <a class="retour-haut" href="#up">Retour haut</a>
+                    </div>
+                    <div class="panel-body">
+
+                    </div>
+                </div>
+            </div>
+            
+
+            <br>
+
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="js/jquery.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="js/bootstrap.js"></script>
+    </body>
+</html>
---
+++ langues.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Guillaume Dalle"/>
+        <meta name="keywords" content="Mots clefs relatifs à cette page"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSS Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- CSS Perso -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <title>Cours de langues</title>
+
+    </head>
+
+    <body>
+
+        <div class="container fluid">
+
+            <div class="row">
+                <div class="page header">
+                    <h1>Cours de langues
+                        <br>
+                        <small>Accueil</small>
+                    </h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <a href="index.php"><button type="button" class="btn btn-danger">Retour Assistant DE</button></a>
+            </div>
+
+            <br>
+
+            <div class="row">                    
+                <ul class="nav nav-pills">
+                    <li role="presentation" class="active"><a href="langues.php">Accueil</a></li>
+                    <li role="presentation"><a href="description.php">Choix des cours</a></li>
+                    <li role="presentation"><a href="#">Mon affectation</a></li>
+                </ul>
+            </div>
+
+            <br>
+
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <p class="citation">"If you talk to a man in a language he understands, that goes to his head. If you talk to him in his own language, that goes to his heart."</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 col-sm-offset-6">
+                    <p>Nelson Mandela</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div class="row">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Fonctionnement</h3>
+                    </div>
+                    <div class="panel-body">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+            </div>
+
+            <br>
+
+        </div>
+
+        <br>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
