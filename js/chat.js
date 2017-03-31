22525e8f69e8f444a78f0e88b093c9069b9f60ae
gdalle <guillaume.c.dalle@gmail.com>
2017-03-31 14:14:15 +0200

    Chat perché

--- js/chat.js
+++ js/chat.js
@@ -14,8 +14,13 @@ $(document).ready(function () {
 
     function displayMessages()
     {
-        $.get('utilities/chatHandler.php?todo=getMessages', 'false', function(data) {
-        $('#chatbox').html(data); $('#chatbox').scrollTop($('#chatbox').get(0).scrollHeight);
+          $.get('utilities/chatHandler.php?todo=getMessages', 'false', function(data) {
+          var oldText = $("#chatbox").html();
+          if(oldText !== data)
+          {
+            $('#chatbox').html(data);
+            $('#chatbox').scrollTop($('#chatbox').get(0).scrollHeight);
+          }
       }, 'text');
     }
 
--- utilities/chatHandler.php
+++ utilities/chatHandler.php
@@ -20,19 +20,18 @@ if (isset($_GET['todo']) && $_GET['todo'] == 'sendMessage') {
     $UID = $_SESSION["userId"];
     $cabinet = $_SESSION["cabinet"];
     $db = MyDatabase::connect();
-    $req = $db->prepare("INSERT INTO chat (user, message_date, cabinet, message) VALUES(?, ?, ?, ?)");
-    $success = $req->execute(array($UID, date("H:i"), $cabinet, $message));
+    $req = $db->prepare("INSERT INTO chat (user, cabinet, message) VALUES(?, ?, ?)");
+    $success = $req->execute(array($UID, $cabinet, $message));
 }
 
 if (isset($_GET['todo']) && $_GET['todo'] == 'getMessages') {
     $db = MyDatabase::connect();
-    $messages = $db->query("SELECT * FROM chat INNER JOIN users ON chat.user = users.id WHERE chat.cabinet = " . $_SESSION['cabinet'] . ";");
+    $messages = $db->query("SELECT * FROM chat INNER JOIN users ON chat.user = users.id WHERE chat.cabinet = " . $_SESSION['cabinet'] . " ORDER BY chat.message_date ASC;");
     while ($message = $messages->fetch()) {
 
         echo "<p>";
-        echo "<b> " . $message['name'] . " </b> <br/>";
+        echo "<b> " . $message['name'] . " </b> (". $message["message_date"]. ") <br>";
         echo($message['message']);
         echo "</p>";
     }
 }
-
