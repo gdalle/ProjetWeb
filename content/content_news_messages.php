67feb1eb830d14f1fcda8fa50a05f7a65d5ee5ca
gdalle <guillaume.c.dalle@gmail.com>
2017-03-31 14:11:27 +0200

    News 2 - ca commence à m'enerver

--- content/content_news_messages.php
+++ content/content_news_messages.php
@@ -1,28 +1,7 @@
 <div class="row">
-    <div class="col-sm-4">
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title">Newsfeed</h3>
-            </div>
-            <div class="panel-body">
-                <ul class="list-group">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item list-group-item-info">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Morbi leo risus</li>
-                    <li class="list-group-item">Porta ac consectetur ac</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-4">
-        <div class="panel panel-default">
-            <div class="panel-heading"><h3 class="panel-title">Dapibus ac facilisis in</h3></div>
-            <div class="panel-body">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-            </div>
-        </div>
-    </div>
+  <?php require('utilities/newsHandler.php');
+    displayNews();
+  ?>
     <div class="col-sm-4">
         <div class="panel panel-danger">
             <div class="panel-heading">
